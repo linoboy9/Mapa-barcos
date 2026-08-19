b94ffe126e3e3810b4ebde37e670996ba97136eb
linoboy9 <142373926+linoboy9@users.noreply.github.com>
2026-08-19 15:04:17 -0400

    Update barcos.py

--- barcos.py
+++ barcos.py
@@ -7,7 +7,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}}
+CORS(app)  # Simplificado para evitar cualquier error de sintaxis
 
 API_KEY = os.environ.get("AIS_API_KEY")
 
